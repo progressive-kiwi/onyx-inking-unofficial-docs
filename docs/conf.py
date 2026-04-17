72bb1c58460b414d31371b850fc5ba587abc3a9b
progressive-kiwi <it.was.working.yesterday@gmail.com>
2026-04-17 17:44:13 +0200

    timestamp, so people know how old this docs is

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,10 @@
+import datetime
+_build_date = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d')
+
 project   = 'Unofficial Onyx SDK Docs'
 copyright = f'2026, Unofficial Onyx SDK Docs contributors. Generated {_build_date}'
 author    = 'Unofficial Onyx SDK Docs contributors'
 release   = '1.4.12'
-
-import datetime
-_build_date = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d')
 
 extensions = []
 
